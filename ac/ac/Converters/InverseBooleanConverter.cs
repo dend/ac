39c22a86750ecef7b0ffb62c0bb3804cc9a5d174
Den Delimarsky <1389609+dend@users.noreply.github.com>
2021-12-21 11:03:04 -0800

    Update state binding

---
+++ ac/ac/Converters/InverseBooleanConverter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows.Data;
+
+namespace ac.Converters
+{
+    public class InverseBooleanConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return targetType != typeof(bool) ? throw new InvalidOperationException("The target must be a Boolean.") : !(bool)value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
